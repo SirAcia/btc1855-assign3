6e02340408c5d74c5916d0f85b8bb635340acce9
Crystal Lee <crystallee@Crystals-MacBook-Pro.local>
2024-06-29 17:44:25 -0400

    Added masking function and updated game loop

--- btc1855-assign3.R
+++ btc1855-assign3.R
@@ -1,7 +1,7 @@
 # BTC1855 CODING IN R 
 # ASSIGNMENT THREE 
 # CRYSTAL LEE
-# Version 2023.03.1+446
+# Version 2024.04.2+764 
 
 # Make text file with words - 
 # Choose a random word from the list - 
@@ -14,7 +14,9 @@
 # If yes, let user know and how many tries they have left
 # If not, let user know and how many tries they have left
 # If they ran out of tries, exit program
+library(ggplot2)
 
+# Welcome and instructions
 while (TRUE) {
 cont <- readline(prompt = "Welcome to hangman! Click R to continue: ")
 if (cont == "R" | cont == "r") {
@@ -28,11 +30,15 @@ if (cont == "R" | cont == "r") {
   }
 } 
         
+# Load word list and select a word
 while (TRUE) {
   cont2 <- readline(prompt = "Click R to continue: ")
-  if (cont == "R" | cont == "r") {
+  if (cont2 == "R" | cont2 == "r") {
     wordlist <- read.delim("assign3-wordlist.txt", header = F)
     word <- sample(wordlist$V1, 1)
+    masked_word <- rep("_", nchar(word))
+    lives <- 3
+    guessed_letters <- character()
     print(paste("Your word has", nchar(word), "characters!"))
     break
   } else {
@@ -40,7 +46,33 @@ while (TRUE) {
   }
 } 
 
+update_masked_word <- function(word, masked_word, guess) {
+  for (i in seq_along(word)) {
+    if (substr(word, i, i) == guess) {
+      masked_word[i] <- guess
+    }
+  }
+  return(masked_word)
+}
+
+# Game loop
+while (TRUE) {
+  print(paste(masked_word, collapse = " "))
+  cont3 <- readline(prompt = "Guess a letter: ")
+  cont3 <- to lower(cont3) # Change all letters to lower case 
+ 
+  if (cont3 %in% guessed_letters) {
+    print("You already guessed that! Try again")
+    print(paste(masked_word, collapse = " "))
+    next
+  } else {
+    print("Not quite! You have 2 tries left.") 
+  }
+} 
 
 
 
-      
+# DIFFICLUTY LEVELS
+# ACTUAL HANGMAN 
+
+
