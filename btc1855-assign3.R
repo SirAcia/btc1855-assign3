e0c1955f68a6a12c33eda92fb519c6f7143573b6
Crystal Lee <crystallee@Crystals-MacBook-Pro.local>
2024-07-01 16:26:06 -0400

    Added comments

--- btc1855-assign3.R
+++ btc1855-assign3.R
@@ -16,6 +16,7 @@
 # If they ran out of tries, exit program
 
 # Please download the assign3-wordlist.txt file from my repo :)
+# Click source to begin
 
 # Welcome and instructions
 intro <- function() {
@@ -96,13 +97,13 @@ game <- function() {
           # Create an empty character vector to store all the guessed letters
           guessed_letters <- character()
           print(paste("Your word has", nchar(word), "characters!"))
-          break
+          break 
         } else {
           # If user doesn't enter R to continue
           print("Oops! Are you sure you entered the right key?") 
         }
       }
-      break
+      break # Get out of first while loop
     } else {
       print("Oops! Are you sure you entered the right key?") 
     } 
@@ -110,69 +111,80 @@ game <- function() {
   
   # Game loop
   while (TRUE) {
+    # Print the masked word and separate underscores with a space
     print(paste(masked_word, collapse = " "))
     cont3 <- readline(prompt = "Guess a letter or a word: ")
     cont3 <- tolower(cont3)  # Change all letters to lower case 
     
-    if (nchar(cont3) != 1 && nchar(cont3) != nchar(word)) { # If the entry is not a single letter or word that is the same length
+    # If the entry is not a single letter or word that is the same length
+    if (nchar(cont3) != 1 && nchar(cont3) != nchar(word)) { 
       print("Please enter a single letter or guess a word.")
       next
     }
     
-    if (cont3 %in% guessed_letters) { # No repeat guesses
+    # No repeat guesses
+    if (cont3 %in% guessed_letters) { 
       print("You already guessed that! Try again.")
       next
     } 
     
+    # Combine all the prev guessed letters with the new guessed letter and store it
     guessed_letters <- c(guessed_letters, cont3)
     
-    if (nchar(cont3) == nchar(word)) { # Check if guessed word is same length
-      if (cont3 == word) { # Check if guess is the word
-        masked_word <- strsplit(word, "")[[1]] # Show all correct letters in the masked word
+    # Check if guessed word is same length
+    if (nchar(cont3) == nchar(word)) { 
+      # Check if the guessed word is the word
+      if (cont3 == word) { 
+        # Show all correct letters in the masked word 
+        masked_word <- strsplit(word, "")[[1]] 
         print("Congrats! You guessed the word!")
-        break
+        break # Exit the game if the word is correct
       } else {
+        # Decrease number of tries if guessed word isn't correct
         lives <- lives - 1
         if (lives != 1) {
           print(paste("Not quite! You have", lives, "tries left. Try again."))
         } else {
-          print("Not quite! You have 1 try left. Try again.") # Grammar for singular try
+          # Grammar for singular try
+          print("Not quite! You have 1 try left. Try again.") 
         }
       }
     } else {
-      match <- grepl(cont3, word, ignore.case = TRUE)  # Check if the guessed letter is in the word
+      # Check if the guessed letter is in the word
+      match <- grepl(cont3, word, ignore.case = TRUE) 
       
       if (any(match)) {
-        masked_word <- update_masked_word(word, masked_word, cont3) # If guessed letter is in word, display in masked word
+        # If guessed letter is in word, display in masked word
+        masked_word <- update_masked_word(word, masked_word, cont3) 
         print("You got it!")
       } else {
         lives <- lives - 1
         if (lives != 1) {
           print(paste("Not quite! You have", lives, "tries left. Try again."))
         } else {
-          print("Not quite! You have 1 try left. Try again.") # Grammar for singular try
+          print("Not quite! You have 1 try left. Try again.") 
         }
       }
     }
     
     if (!("_" %in% masked_word)) {
       print(paste("Congrats! You guessed the word:", word))
-      break
+      break # Exit game if word is guessed
     } else if (lives == 0) {
       print(paste("You ran out of tries! The word was:", word))
-      break
+      break # Exit game if user runs out of tries
     }
   }
 }
 
-
-repeat { # Loop to repeat game at the end 
+repeat { 
   intro()
   game()
+  
   restart <- readline(prompt = "Do you want to play again? (Y/N): ")
+  # If user doesn't choose Y
   if (tolower(restart) != "y") {
     print("Thank you for hanging out! Have a nice day :)")
     break
   }
 }
-
